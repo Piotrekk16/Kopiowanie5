68a2c1bfc025b7016f905f36e6d1923fb5811edb
Piotrekk85 <piotr.jastr@gmail.com>
2016-11-18 18:28:26 +0100

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3/Osoba.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3 
+{
+    class Osoba : ICloneable
+    {
+        private int wiek; //deklaracja pola
+        public Urodziny ur; //pole referencyjne
+
+        //konstruktor domyślny (uzupełniamy o tworzenie obiektu)
+        public Osoba()
+        {
+            ur = new Urodziny();
+        }
+
+        //konstruktor parametryczny
+        public Osoba(int wiek,string miejsce) 
+        {
+            this.wiek = wiek;
+            ur = new Urodziny();
+            ur.UstawMiejsceUr(miejsce);
+        }
+
+        public void UstawWiek(int wiek) //metoda na ustawienie wieku
+        {
+            this.wiek = wiek;
+        }
+
+        public int PobierzWiek() //metoda na pobranie wieku
+        {
+            return wiek;
+        }
+
+        public Object Clone() //implementacja metody Clone
+        {
+            return MemberwiseClone();
+        }
+
+    }
+}
